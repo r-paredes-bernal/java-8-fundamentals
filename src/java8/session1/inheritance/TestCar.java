32d16f67a933e329415db3217270e1d2c023b7df
Rafael Paredes <Rafael.X.Paredes.-ND@disney.com>
2019-02-19 15:51:43 -0600

    Session 1 - First Class

---
+++ src/java8/session1/inheritance/TestCar.java
@@ -0,0 +1,20 @@
+package java8.session1.inheritance;
+
+/* Open the image file img/Java8-Session1-Class Diagram.jpg and create 
+ * the objects and interfaces required.
+ * 
+ * 1.- Create a constructor in Car that receives all the arguments to initialize them  
+ * 2.- Create toString Method to display all the attributes
+ * 3.- Create an instance of car and use the constructor in number 1 to initialize the attributes
+ * 4.- Call the toString method and run the class to see the output 
+ * 
+ * Note: Fix the issues if there is any 
+ * 
+ */
+public class TestCar {
+
+	public static void main(String[] args) {
+
+	}
+
+}
