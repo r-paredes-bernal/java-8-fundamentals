b25dba8830c5e62fcde3ebb3ebcd86d3db360031
Ramos Croda <v.ramos.croda@accenture.com>
2019-02-21 12:22:07 -0600

    fixed error

--- src/java8/session1/inheritance/ElectricCar.java
+++ src/java8/session1/inheritance/ElectricCar.java
@@ -1,11 +1,18 @@
 package java8.session1.inheritance;
 
+import java.util.Date;
+
 /**
  * 
  * @author v.ramos.croda
  * This class is using from electric car.
  */
 public class ElectricCar extends Car {
+
+	public ElectricCar(Long id, String name, Integer year, String color, Date date) {
+		super(id, name, year, color, date);
+		// TODO Auto-generated constructor stub
+	}
 
 	/*variable for batteries*/
 	public Integer batteries;
--- src/java8/session1/inheritance/GasolineCar.java
+++ src/java8/session1/inheritance/GasolineCar.java
@@ -1,7 +1,14 @@
 package java8.session1.inheritance;
 
+import java.util.Date;
+
 public class GasolineCar extends Car{
 	
+	public GasolineCar(Long id, String name, Integer year, String color, Date date) {
+		super(id, name, year, color, date);
+		// TODO Auto-generated constructor stub
+	}
+
 	public Integer gallons;
 	
 	public Float mpg;
